161e695b181b7925f528b51e9d3d9b4e91da5bb6
Ali Mehdi <salimehdi144@gmail.com>
2018-12-30 00:41:41 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Home </title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<link href="https://fonts.googleapis.com/css?family=Corben:700|Delius+Unicase:700|Ewert|Frijole|Galada|Holtwood+One+SC|Modak|Ruthie|Shojumaru|Tangerine:700" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Tangerine" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Cookie|Oregano" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Cookie" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Alike" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Almendra" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:600i" rel="stylesheet">
+
+  <link rel="stylesheet" type="text/css" href="css/index.css">
+   <link rel="stylesheet" type="text/css" href="css/footar.css">
+  <style type="text/css">
+    li a{
+      font-family: 'Holtwood One SC', serif;
+       text-shadow: 2px 2px 2px white;
+        float: right;
+        padding-right: 9%;
+        padding-top: 2%;
+        font-size: 38px;
+        color: #066c61;
+        text-decoration : none;
+        
+
+      }
+
+      li a:hover {
+        text-decoration : none;
+        color: white;
+      } 
+
+
+      .glow-button:hover {
+        color: rgba(255, 255, 255, 1);
+        box-shadow: 15px 15px 15px rgba(145, 92, 182, .4);
+        } 
+
+
+      .button {
+        text-decoration: none;
+        color: #066c61;
+        background: #00a69c;
+        padding: 15px 40px;
+        border-radius: 4px;
+        font-weight: normal;
+        text-transform: uppercase;
+        transition: all 0.2s ease-in-out;
+      }
+
+
+
+      
+
+  </style>
+</head>
+
+
+
+<body>
+
+        <div style="  background-size: cover; 
+                      height: 100%;
+                      background-image: url(assets/images/newback1.jpg);
+                      overflow: hidden;
+                      text-align: center;"> 
+
+
+
+<!-- NAV -->
+
+      <ul style="list-style-type: none; margin: 0; padding: 0;">
+      
+        <li style="display: inline; float: left; padding-left: 4%;"><a href="index.php"> <img style="height: 100px; width: 170px;" src="assets/images/logo.png"> </a></li>
+
+        <li ><a href="help.php"> Help </a></li>
+        
+        <li  ><a href="about.php"> About </a></li>
+
+        <li  ><a href="login.php"> Login </a></li>
+      
+      </ul>
+  
+  
+<!-- NAV End -->
+
+
+
+<!-- Jumbotron -->
+    
+
+
+       <div class="jumbotron" style="padding-top: 7%;">
+        
+        <h1 class="display-4" style="font-family: 'Almendra', serif; font-style: bold; font-size: 75px;">DARZI DOT PK</h1>
+        
+        <h2 class="lead" style="font-family: 'Tangerine', cursive; font-size: 50px;"> Aao, Silwaao aur Mazay karo !</h2>
+        
+        <hr class="my-4">
+        
+        <p  style="font-family: 'Alike', serif; font-size: 22px;">Darzi ensures that whatever you wear stands out in the crowd <br> by stitching your favorite dresses out 
+          of the best designs from <br>   our expert darzis in affordable prices </p>
+          <br>
+        
+        <a style="font-family: 'Oregano', cursive; font-size: 15px; color: white" class="button glow-button" href="order.php" role="button">Place An Order</a>
+        
+        <a style="font-family: 'Oregano', cursive; font-size: 15px;color: white" class="button glow-button" href="becomeadarzi.php" role="button">Become A Darzi</a>
+   
+
+
+<br>  <br>  <br>  <br>  <br>   <br> <br>  <br> 
+
+    </div>
+
+
+<!-- Jumbotron ends -->
+
+</div> <!-- bgground image DIV -->
+
+<br> <br><br><br><br><br>
+
+
+
+
+
+
+
+    <div>
+        <div>
+        
+        <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="assets/images/empowering.png"" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title" style="font-family: 'Open Sans', sans-serif;  font-size: 25px;">Empowering People</h5>
+      <p class="card-text" style="font-family: 'Open Sans', sans-serif; font-style: bold italic; font-size: 15px;">Providing many Darzis with full and part time job opportunities to support their families.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted"></small>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="assets/images/impact.png"" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title"  style="font-family: 'Open Sans', sans-serif;  font-size: 25px;">Simplifying Lives</h5>
+      <p class="card-text" style="font-family: 'Open Sans', sans-serif; font-style: bold italic; font-size: 15px;">Getting you a dress in a specific time, so you can enjoy your special moments and occasions</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted"></small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="assets/images/impact.png"" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title"  style="font-family: 'Open Sans', sans-serif;  font-size: 25px;">Simplifying Lives</h5>
+      <p class="card-text" style="font-family: 'Open Sans', sans-serif; font-style: bold italic; font-size: 15px;">Getting you a dress in a specific time, so you can enjoy your special moments and occasions</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted"></small>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="assets/images/impact.png"" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title"  style="font-family: 'Open Sans', sans-serif;  font-size: 25px;">Simplifying Lives</h5>
+      <p class="card-text" style="font-family: 'Open Sans', sans-serif; font-style: bold italic; font-size: 15px;">Getting you a dress in a specific time, so you can enjoy your special moments and occasions</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted"></small>
+    </div>
+  </div>
+
+
+  <div class="card">
+    <img class="card-img-top" src="assets/images/simplify.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title"  style="font-family: 'Open Sans', sans-serif;  font-size: 25px;">Impacting Economies</h5>
+      <p class="card-text" style="font-family: 'Open Sans', sans-serif; font-style: bold italic; font-size: 15px;">Thanks to our Darzis , more than 100  people in the region can get to and from work daily.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted"></small>
+    </div>
+  </div>
+</div>
+
+</div>
+
+</div>
+
+<br><br><br>
+
+<!-- Footer  -->
+
+
+<div style="background-color:#5b686b" class="footar">
+  
+<div class="container">
+  
+
+  <div class="row">
+      
+
+      <div class="col-sm">
+        <br><br><br>
+        <img style=" height: 55%; width: 74%;" src="assets/images/logo.png">
+
+
+      </div>
+
+
+      <div class="col-sm">
+        
+        <br><br><br>
+        <h2> Adress </h2>
+        
+        <p>          
+            Software Engineering <br> Department <br>NED University <br>Karachi.
+        </p>
+      </div>
+
+
+
+      <div class="col-sm">
+        
+        <br><br><br>
+        <h2> Contacts </h2>
+       
+        <p>
+          Email: darzipk@gmail.com 
+          <br>Phone:  0900-223190 
+          <br>Fax:  0334-4430223
+        </p>
+
+      </div>
+
+
+      <div class="col-sm">
+        
+        <br><br><br>
+        <h2>Links</h2>
+        
+           <p>
+            <a href="#">Facebook</a> <br>
+            <a href="#">Linkedin</a> <br>
+            <a href="#">Instagram</a> <br>
+            <a href="#">Snapchat</a>
+           </p>
+        
+      </div>
+
+  </div>
+
+
+
+</div>
+<br><br><br><hr>
+<p style="text-align: center;">© Copyright 2018 Darzi - All Rights Reserved</p>
+<br>
+</div>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
