0221ed90295d23b6639c81d10922e9dcfbab9caf
Ali Mehdi <salimehdi144@gmail.com>
2018-12-30 00:41:38 +0500

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> About Us </title>
+	<link rel="stylesheet" type="text/css" href="css/about.css">
+	<link rel="stylesheet" type="text/css" href="css/nav.css">
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
+	<link rel="stylesheet" type="text/css" href="css/help.css">
+	
+</head>
+
+<body style= "background-size: cover;
+      height: 100%;
+      background-image: url(assets/images/fbg.jpg);
+      overflow: hidden;
+      text-align: center;">
